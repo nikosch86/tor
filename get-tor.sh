3544d121d2dfccdd0f071b5b89a856fb0f3a1041
Niko <niko@nkdev.io>
2020-12-23 08:26:43 +0400

    shellcheck the script

--- get-tor.sh
+++ get-tor.sh
@@ -1,28 +1,27 @@
-#!/bin/bash -eux
+#!/bin/bash -eu
+# shellcheck disable=SC2181,SC2015
+
 curl -fSsL https://dist.torproject.org/ > tor_download_page
 if [ $? -ne 0 ]; then
         echo "Failed getting torproject download page, bailing"
         exit 2
 fi
-CURRENT_TOR_VERSION=$(cat tor_download_page | grep 'tor-.*tar.gz"' | sed -E 's/^.*href="tor-([0-9\.]+)\.tar\.gz".*$/\1/g' | grep -E '^[0-9\.]+$' | sort -V | tail -1)
+CURRENT_TOR_VERSION=$(grep 'tor-.*tar.gz"' tor_download_page | sed -E 's/^.*href="tor-([0-9\.]+)\.tar\.gz".*$/\1/g' | grep -E '^[0-9\.]+$' | sort -V | tail -1)
 rm tor_download_page
 CURRENT_TOR_VERSION_FILENAME=tor-${CURRENT_TOR_VERSION}.tar.gz
 CURRENT_TOR_VERSION_URL=https://dist.torproject.org/${CURRENT_TOR_VERSION_FILENAME}
 CURRENT_TOR_VERSION_SIG_URL=https://dist.torproject.org/${CURRENT_TOR_VERSION_FILENAME}.asc
 echo "found TOR filename: ${CURRENT_TOR_VERSION_FILENAME}"
 echo "downloading signature, adding key"
-# curl -s https://openpgpkey.torproject.org/.well-known/openpgpkey/torproject.org/hu/kounek7zrdx745qydx6p59t9mqjpuhdf |gpg --import -
-# gpg -vv --auto-key-locate nodefault,wkd --locate-keys arma#torproject.org
-# gpg --keyserver pool.sks-keyservers.net --recv-keys 0x28988BF5
 TOR_GPG_KEY=$(gpg --batch --keyserver keys.gnupg.net --search-keys arma@torproject.org | grep 'RSA key' | sed -E 's/^.*RSA key ([0-9A-Z]+), created.*/\1/g')
-gpg --keyserver keys.gnupg.net --recv-keys $TOR_GPG_KEY
+gpg --keyserver keys.gnupg.net --recv-keys "${TOR_GPG_KEY}"
 TOR_GPG_KEY=$(gpg --batch --keyserver keys.gnupg.net --search-keys nickm@torproject.org | grep 'RSA key' | sed -E 's/^.*RSA key ([0-9A-Z]+), created.*/\1/g')
-gpg --keyserver keys.gnupg.net --recv-keys $TOR_GPG_KEY
-curl -fSsLO ${CURRENT_TOR_VERSION_SIG_URL}
+gpg --keyserver keys.gnupg.net --recv-keys "${TOR_GPG_KEY}"
+curl -fSsLO "${CURRENT_TOR_VERSION_SIG_URL}"
 
 DONE=0
 while [ $DONE -eq 0 ]; do
-        curl -fLO ${CURRENT_TOR_VERSION_URL} && tar xf ${CURRENT_TOR_VERSION_FILENAME} --strip-components 1 && gpg --verify ${CURRENT_TOR_VERSION_FILENAME}.asc ${CURRENT_TOR_VERSION_FILENAME}
+        curl -fSsLO "${CURRENT_TOR_VERSION_URL}" && tar xf "${CURRENT_TOR_VERSION_FILENAME}" --strip-components 1 && gpg --verify "${CURRENT_TOR_VERSION_FILENAME}.asc" "${CURRENT_TOR_VERSION_FILENAME}"
         [ $? -eq 0 ] && DONE=1 || {
                 echo "failed getting tor browser from ${CURRENT_TOR_VERSION_URL}, sleeping 2 seconds and retrying"
                 sleep 2
